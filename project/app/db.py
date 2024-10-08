3f89857b3ba821e867b746ccc74ae646825661d9
jahidul <jahidul@nexgencloud.com>
2024-10-08 18:11:24 +0600

    Fixed versions and save data to the database

--- project/app/db.py
+++ project/app/db.py
@@ -1,10 +1,13 @@
+import logging  # new
 import os
-import logging
+
 from fastapi import FastAPI
-from tortoise import Tortoise, run_async
+from tortoise import Tortoise, run_async  # new
 from tortoise.contrib.fastapi import register_tortoise
 
+
 log = logging.getLogger("uvicorn") # new
+
 
 TORTOISE_ORM = {
     "connections": {"default": os.environ.get("DATABASE_URL")},
@@ -16,6 +19,7 @@ TORTOISE_ORM = {
     },
 }
 
+
 def init_db(app: FastAPI) -> None:
     register_tortoise(
         app,
@@ -24,17 +28,21 @@ def init_db(app: FastAPI) -> None:
         generate_schemas=False,
         add_exception_handlers=True,
     )
-    
+
+
+# new
 async def generate_schema() -> None:
     log.info("Initializing Tortoise...")
-    
+
     await Tortoise.init(
-        db_url=os.environ.get('DATABASE_URL'),
+        db_url=os.environ.get("DATABASE_URL"),
         modules={"models": ["models.tortoise"]},
     )
-    log.info("Generating database schema via Tortoise.....")
+    log.info("Generating database schema via Tortoise...")
     await Tortoise.generate_schemas()
     await Tortoise.close_connections()
-    
+
+
+# new
 if __name__ == "__main__":
     run_async(generate_schema())
--- project/app/main.py
+++ project/app/main.py
@@ -1,27 +1,32 @@
-import os
-import logging
-from fastapi import FastAPI
-from tortoise.contrib.fastapi import register_tortoise
 
-from app.apis import ping
+import logging
+
+from fastapi import FastAPI
+
+from app.apis import ping, summaries  # updated
 from app.db import init_db
-app = FastAPI()
+
 
 log = logging.getLogger("uvicorn")
 
+
 def create_application() -> FastAPI:
     application = FastAPI()
-    # include router
     application.include_router(ping.router)
+    application.include_router(summaries.router, prefix="/summaries", tags=["summaries"])  # new
+
     return application
 
+
 app = create_application()
+
 
 @app.on_event("startup")
 async def startup_event():
     log.info("Starting up...")
     init_db(app)
-    
+
+
 @app.on_event("shutdown")
 async def shutdown_event():
     log.info("Shutting down...")
