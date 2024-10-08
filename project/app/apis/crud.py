ff674e931e128905c88aaa09394a75029ed201c6
jahidul <jahidul@nexgencloud.com>
2024-10-08 22:37:33 +0600

    Added multiple routes and test files

--- project/app/apis/crud.py
+++ project/app/apis/crud.py
@@ -1,3 +1,4 @@
+from typing import Union, List
 from app.models.pydantic import SummaryPayloadSchema
 from app.models.tortoise import TextSummary
 
@@ -9,3 +10,10 @@ async def post(payload: SummaryPayloadSchema) -> int:
     )
     await summary.save()
     return summary.id
+
+async def get(id: int) -> Union[dict, None]:
+    return await TextSummary.filter(id=id).first()
+
+async def get_all() -> List:
+    summaries = await TextSummary.all().values()
+    return summaries
