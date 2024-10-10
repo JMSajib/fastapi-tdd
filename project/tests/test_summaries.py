e119f64abf3337a16bb15abfc70b18a39c81d3ab
jahidul <jahidul@nexgencloud.com>
2024-10-10 11:24:41 +0600

    Added PUT and delete api and prod docker file

--- project/tests/test_summaries.py
+++ project/tests/test_summaries.py
@@ -22,6 +22,10 @@ def test_create_summaries_invalid_json(test_app):
     #         }
     #     ]
     # }
+    
+    response = test_app.post("/summaries/", data=json.dumps({"url": "invalid://url"}))
+    assert response.status_code == 422
+    assert response.json()["detail"][0]["msg"] == "URL scheme should be 'http' or 'https'"
 
 
 def test_read_summary(test_app_with_db):
@@ -41,6 +45,25 @@ def test_read_summary_incorrect_id(test_app_with_db):
     response = test_app_with_db.get("/summaries/999/")
     assert response.status_code == 404
     assert response.json()["detail"] == "Summary not found"
+
+    response = test_app_with_db.get("/summaries/999/")
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Summary not found"
+
+    response = test_app_with_db.get("/summaries/0/")
+    assert response.status_code == 422
+    # assert response.json() == {
+    #     "detail": [
+    #         {
+    #             "ctx": {"gt": 0},
+    #             "input": "0",
+    #             "loc": ["path", "id"],
+    #             "msg": "Input should be greater than 0",
+    #             "type": "greater_than",
+    #             "url": "https://errors.pydantic.dev/2.5/v/greater_than",
+    #         }
+    #     ]
+    # }
     
     
 def test_read_all_summaries(test_app_with_db):
@@ -52,4 +75,107 @@ def test_read_all_summaries(test_app_with_db):
 
     response_list = response.json()
     assert len(list(filter(lambda d: d["id"] == summary_id, response_list))) == 1
+    
 
+def test_remove_summary(test_app_with_db):
+    response = test_app_with_db.post(
+        "/summaries/", data=json.dumps({"url": "https://foo.bar"})
+    )
+    summary_id = response.json()["id"]
+
+    response = test_app_with_db.delete(f"/summaries/{summary_id}/")
+    assert response.status_code == 200
+    assert response.json() == {"id": summary_id, "url": "https://foo.bar"}
+
+
+def test_remove_summary_incorrect_id(test_app_with_db):
+    response = test_app_with_db.delete("/summaries/999/")
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Summary not found"
+
+def test_update_summary(test_app_with_db):
+    response = test_app_with_db.post(
+        "/summaries/", data=json.dumps({"url": "https://foo.bar"})
+    )
+    summary_id = response.json()["id"]
+
+    response = test_app_with_db.put(
+        f"/summaries/{summary_id}/",
+        data=json.dumps({"url": "https://foo.bar", "summary": "updated!"})
+    )
+    assert response.status_code == 200
+
+    response_dict = response.json()
+    assert response_dict["id"] == summary_id
+    assert response_dict["url"] == "https://foo.bar"
+    assert response_dict["summary"] == "updated!"
+    assert response_dict["create_at"]
+
+
+def test_update_summary_incorrect_id(test_app_with_db):
+    response = test_app_with_db.put(
+        "/summaries/999/",
+        data=json.dumps({"url": "https://foo.bar", "summary": "updated!"})
+    )
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Summary not found"
+
+
+def test_update_summary_invalid_json(test_app_with_db):
+    response = test_app_with_db.post(
+        "/summaries/", data=json.dumps({"url": "https://foo.bar"})
+    )
+    summary_id = response.json()["id"]
+
+    response = test_app_with_db.put(
+        f"/summaries/{summary_id}/",
+        data=json.dumps({})
+    )
+    assert response.status_code == 422
+    # assert response.json() == {
+    #     "detail": [
+    #         {
+    #             "input": {},
+    #             "loc": ["body", "url"],
+    #             "msg": "Field required",
+    #             "type": "missing",
+    #             "url": "https://errors.pydantic.dev/2.5/v/missing",
+    #         },
+    #         {
+    #             "input": {},
+    #             "loc": ["body", "summary"],
+    #             "msg": "Field required",
+    #             "type": "missing",
+    #             "url": "https://errors.pydantic.dev/2.5/v/missing",
+    #         }
+    #     ]
+    # }
+
+
+def test_update_summary_invalid_keys(test_app_with_db):
+    response = test_app_with_db.post(
+        "/summaries/", data=json.dumps({"url": "https://foo.bar"})
+    )
+    summary_id = response.json()["id"]
+
+    response = test_app_with_db.put(
+        f"/summaries/{summary_id}/",
+        data=json.dumps({"url": "https://foo.bar"})
+    )
+    assert response.status_code == 422
+    # assert response.json() == {
+    #     "detail": [
+    #         {
+    #             "loc": ["body", "summary"],
+    #             "msg": "field required",
+    #             "type": "value_error.missing",
+    #         }
+    #     ]
+    # }
+
+    response = test_app_with_db.put(
+        f"/summaries/{summary_id}/",
+        data=json.dumps({"url": "invalid://url", "summary": "updated!"})
+    )
+    assert response.status_code == 422
+    assert response.json()["detail"][0]["msg"] == "URL scheme should be 'http' or 'https'"
